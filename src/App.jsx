6070877ac6e95c948fb090aaa9f5d2d7e32d855f
tnleibel <tnleibel@outlook.com>
2024-08-13 07:30:14 -0400

    form and index pages created by shalom, committing to merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import * as authService from '../src/services/authService'
-import Register from "./Register/Register";
-import SignIn from "./SignIn/SignIn";
+import Register from "./components/Register/Register";
+import SignIn from "./components/SignIn/SignIn";
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser())
 
   return (
-
-    <div>
-        <h1>Hello world!</h1>
-        <Register />
-    </div>
-    <Routes>
-      <Route path='/' element={<Landing />} />
-      <Route path='sign-up' element={<SignupForm />} />
-      <Route path='sign-in' element={<SigninForm />} />
-    </Routes>
-
+    <>
+      <div>
+          <h1>Hello world!</h1>
+      </div>
+      <NavBar user={user} setUser={setUser}/>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='sign-up' element={<Register setUser={setUser} />} />
+        <Route path='sign-in' element={<SignIn setUser={setUser} />} />
+      </Routes>
+    </>
   );
 }
 
