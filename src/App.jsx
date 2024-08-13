f97b4261ac631c61f26b2690d442321d502e99dd
tnleibel <tnleibel@outlook.com>
2024-08-13 08:42:57 -0400

    added restaurantService to handle all basic frontend CRUD operations for restaurant objects, added route in app.jsx, adjusted RestaurantForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import * as authService from '../src/services/authService'
-import Register from "./Register/Register";
-import SignIn from "./SignIn/SignIn";
-import NavBar from './NavBar/NavBar';
-import Restaurant from './Restaurant.jsx/Restaurant';
+import * as restaurantService from '../src/services/restaurantService'
+import Register from "./components/Register/Register";
+import SignIn from "./components/SignIn/SignIn";
+import NavBar from './components/NavBar/NavBar';
+import Restaurant from './components/Restaurant';
+import RestaurantForm from './components/RestaurantForm/RestaurantForm';
+
 const App = () => {
   const [user, setUser] = useState(authService.getUser())
+  const [restaurants, setRestaurants] = useState([])
+
+  const handleSignout = () => {
+    authService.signout()
+    setUser(null)
+  }
+
+  const handleAddRestaurant = async (restaurantData) => {
+    const newRestaurant = await restaurantService.create(restaurantData)
+    setRestaurants([newRestaurant, ...restaurants])
+  }
   
   return (
     <>
-      <NavBar user={user} setUser={setUser} />
+      <NavBar user={user} setUser={setUser} handleSignout={handleSignout} />
       <Routes>
         {/* <Route path='/' element={<Landing />} /> */}
-        <Route path='sign-up' element={<Register />} />
-        <Route path='sign-in' element={<SignIn />} />
-        <Route path='landing' element={<Restaurant />} />
+        <Route path='/sign-up' element={<Register />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/restaurants' element={<Restaurant />} />
+        <Route path='/restaurants/new' element={<RestaurantForm handleAddRestaurant={handleAddRestaurant} />} />
       </Routes>
     </> 
   );
--- src/components/RestaurantForm/RestaurantForm.jsx
+++ src/components/RestaurantForm/RestaurantForm.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-const RestaurantForm = () => {
+const RestaurantForm = (props) => {
     const [restaurant, setRestaurant] = useState({
         name: '', 
         category: '',
@@ -12,29 +12,38 @@ const RestaurantForm = () => {
     const navigate = useNavigate();
     const {id} = useParams();
 
+    const handleChange = (event) => {
+        setRestaurant({ ...restaurant, [event.target.name]: event.target.value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.handleAddRestaurant(restaurant)
+    }
+
     // will need to fetch restaurant details after backend is done
     useEffect(() => {
 
     });
     // need to write the handle submit and also setting each input onchange to events value
     return (
-        <form onSubmit={}>
+        <form onSubmit={handleSubmit}>
 
             <div>
-                <label>Restaurant Name:</label>
-                <input value={restaurant.name} />
+                <label htmlFor="name">Restaurant Name:</label>
+                <input type="text" name="name" id="name" value={restaurant.name} onChange={handleChange} />
             </div>
             <div>
-                <label>Category:</label>
-                <input value={restaurant.category} />
+                <label htmlFor="category">Category:</label>
+                <input type="text" name="category" id="category" value={restaurant.category} onChange={handleChange} />
             </div>
             <div>
                 <label>Rating:</label>
                 <input value={restaurant.rating} />
             </div>
             <div>
-                <label>Review:</label>
-                <input value={restaurant.review} />
+                <label htmlFor="review">Review:</label>
+                <input type="text-area" name="review" id="review" value={restaurant.review} onChange={handleChange} />
             </div>
             <button type="submit">Add Restaurant</button>
         </form>
